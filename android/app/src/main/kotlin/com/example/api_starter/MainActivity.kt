221bebebc4979412de564d6e8e8c490728534acc
mjstokes86 <mjstokes1986@att.net>
2020-07-24 10:55:54 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/api_starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.api_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/notes_services.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:api_starter/models/note_for_listing.dart';
+import 'package:api_starter/models/api_response.dart';
+import 'package:http/http.dart' as http;
+
+class NotesService {
+  static const API = 'http://api.notes.programmingaddict.com';
+  static const headers = {'apiKey': '9e3d2b56-a58c-4f2f-a947-785adeeedfd8'};
+
+  Future<APIResponse<List<NoteForListing>>> getNotesList() {
+    return http.get(API + '/notes', headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        final notes = <NoteForListing>[];
+        for (var item in jsonData) {
+          final note = NoteForListing(
+            noteID: item['noteID'],
+            noteTitle: item['noteTitle'],
+            createDateTime: DateTime.parse(item['createDateTime']),
+            latestEditDateTime: item['latestEditDateTime'] != null
+                ? DateTime.parse(item['latestEditDateTime'])
+                : null,
+          );
+          notes.add(note);
+        }
+        return APIResponse<List<NoteForListing>>(data: notes);
+      }
+      return APIResponse<List<NoteForListing>>(
+          error: true, errorMessage: 'An error occured');
+    }).catchError((_) => APIResponse<List<NoteForListing>>(
+        error: true, errorMessage: 'An error occured'));
+  }
+}
